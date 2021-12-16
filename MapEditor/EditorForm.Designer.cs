40752b28ad08f10e7fe6396f8ba367c2b93becd9
Никита Антоненко <nikita.dmitrovich@gmail.com>
2021-12-17 00:44:51 +0200

    Add project files.

---
+++ MapEditor/EditorForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace MapEditor
+{
+    partial class EditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MapPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.CategoryBox = new System.Windows.Forms.ComboBox();
+            this.CategoryLb = new System.Windows.Forms.Label();
+            this.EraseCheck = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // MapPanel
+            // 
+            this.MapPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(173)))), ((int)(((byte)(145)))));
+            this.MapPanel.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.MapPanel.Location = new System.Drawing.Point(9, 10);
+            this.MapPanel.Name = "MapPanel";
+            this.MapPanel.Size = new System.Drawing.Size(600, 600);
+            this.MapPanel.TabIndex = 0;
+            this.MapPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MapPanel_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(144)))), ((int)(((byte)(138)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(736, 587);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(144)))), ((int)(((byte)(138)))));
+            this.ClearBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClearBtn.Location = new System.Drawing.Point(615, 587);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(115, 23);
+            this.ClearBtn.TabIndex = 1;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            // 
+            // CategoryBox
+            // 
+            this.CategoryBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CategoryBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CategoryBox.FormattingEnabled = true;
+            this.CategoryBox.Items.AddRange(new object[] {
+            "Grass",
+            "Dirt",
+            "Rock",
+            "Tree",
+            "Sand",
+            "Water"});
+            this.CategoryBox.Location = new System.Drawing.Point(615, 38);
+            this.CategoryBox.Name = "CategoryBox";
+            this.CategoryBox.Size = new System.Drawing.Size(228, 24);
+            this.CategoryBox.TabIndex = 2;
+            // 
+            // CategoryLb
+            // 
+            this.CategoryLb.AutoSize = true;
+            this.CategoryLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CategoryLb.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.CategoryLb.Location = new System.Drawing.Point(615, 19);
+            this.CategoryLb.Name = "CategoryLb";
+            this.CategoryLb.Size = new System.Drawing.Size(71, 16);
+            this.CategoryLb.TabIndex = 3;
+            this.CategoryLb.Text = "Category";
+            // 
+            // EraseCheck
+            // 
+            this.EraseCheck.AutoSize = true;
+            this.EraseCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EraseCheck.Location = new System.Drawing.Point(615, 68);
+            this.EraseCheck.Name = "EraseCheck";
+            this.EraseCheck.Size = new System.Drawing.Size(67, 20);
+            this.EraseCheck.TabIndex = 4;
+            this.EraseCheck.Text = "Eraser";
+            this.EraseCheck.UseVisualStyleBackColor = true;
+            // 
+            // EditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(151)))), ((int)(((byte)(136)))));
+            this.ClientSize = new System.Drawing.Size(855, 617);
+            this.Controls.Add(this.EraseCheck);
+            this.Controls.Add(this.CategoryLb);
+            this.Controls.Add(this.CategoryBox);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MapPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "EditorForm";
+            this.Text = "Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MapPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.ComboBox CategoryBox;
+        private System.Windows.Forms.Label CategoryLb;
+        private System.Windows.Forms.CheckBox EraseCheck;
+    }
+}
+
---
+++ MapEditor/EditorForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapEditor
+{
+    public partial class EditorForm : Form
+    {
+        public EditorForm()
+        {
+            InitializeComponent();
+            this.CategoryBox.SelectedIndex = 0;
+        }
+
+     
+
+        private void MapPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) {
+                Button sqr = new Button();
+                sqr.Location = new Point((e.Location.X/12)*12, (e.Location.Y / 12) * 12);
+                switch (this.CategoryBox.SelectedItem)
+                {
+                    case "Grass":
+                        sqr.Size = new Size(12, 12);
+                        sqr.BackColor = Color.Green;
+                        break;
+                    case "Dirt":
+                        sqr.Size = new Size(12, 12);
+                        sqr.BackColor = Color.Brown;
+                        break;
+                    case "Rock":
+                        sqr.Size = new Size(12, 12);
+                        sqr.BackColor = Color.Gray;
+                        break;
+                    case "Tree":
+                        sqr.Size = new Size(12, 12);
+                        sqr.BackColor = Color.DarkGreen;
+                        break;
+                    case "Sand":
+                        sqr.Size = new Size(12, 12);
+                        sqr.BackColor = Color.Yellow;
+                        break;
+                    case "Water":
+                        sqr.Size = new Size(12, 12);
+                        sqr.BackColor = Color.Blue;
+                        break;
+                    default:
+                        break;
+                }
+                sqr.MouseMove += Sqr_MouseMove;
+                sqr.FlatStyle = FlatStyle.Flat;
+                sqr.FlatAppearance.BorderSize = 0;
+
+                if (!this.EraseCheck.Checked)
+                {
+                    if (!MapPanel.Controls.OfType<Button>().Any((item) => new Rectangle(item.Location, item.Size).IntersectsWith(new Rectangle(sqr.Location, sqr.Size)))
+                                        && (sqr.Location.X >= 0 && ((sqr.Location.X - sqr.Size.Width) < this.MapPanel.Width))
+                                        && (sqr.Location.Y >= 0 && ((sqr.Location.Y - sqr.Size.Height) < this.MapPanel.Height))
+                        )
+                    {
+                        this.MapPanel.Controls.Add(sqr);
+                        this.Text = this.MapPanel.Controls.Count.ToString();
+
+                    }
+                }
+            }
+        }
+
+        private void Sqr_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.EraseCheck.Checked && e.Button == MouseButtons.Left) {
+                this.MapPanel.Controls.Remove(sender as Control);
+            }
+        }
+    }
+}
